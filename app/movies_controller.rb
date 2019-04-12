50005c4fed573d0beb67e401afde9e7155adead9
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-12 02:21:41 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = title: "Home Alone", release_date: 1990)
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
@@ -46,7 +46,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.size
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
